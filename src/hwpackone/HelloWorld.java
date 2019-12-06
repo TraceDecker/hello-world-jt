6a0d215bd9cb945bb8e688439ced956d4f1fcfc0
Trace -- <decker1041@gmail.com>
2019-12-05 18:49:43 -0700

    Initial commit

---
+++ src/hwpackone/HelloWorld.java
@@ -0,0 +1,13 @@
+package hwpackone;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    salute((args.length > 0) ? args[0] : "World");
+  }
+
+  private static void salute(String target) {
+    System.out.println("Hello " + target + "!");
+  }
+
+}
